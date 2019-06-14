ebea0d0566af5a02a1b90e3a30c855f39ee1a64b
Mark <mrk.rosenberg@gmail.com>
2019-06-14 10:16:30 -0600

    adds logic to display calendar/posters on events component

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -9,7 +9,20 @@ export class EventsComponent implements OnInit {
 
   constructor() { }
 
+  poster = true;
+  calendar = false;
+
   ngOnInit() {
+  }
+
+  posterView() {
+    this.poster = true;
+    this.calendar = false;
+  }
+
+  calendarView() {
+    this.calendar = true;
+    this.poster = false;
   }
 
 }
