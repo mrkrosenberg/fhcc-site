522bff7881165b9903cabb118bf9c48623a8457e
Mark <mrk.rosenberg@gmail.com>
2019-06-17 11:49:27 -0600

    adds styling/logic to news comp, updates nav links style

--- src/app/news-component/news-component.component.ts
+++ src/app/news-component/news-component.component.ts
@@ -9,7 +9,54 @@ export class NewsComponentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  featureStory = {
+    title : ``,
+    content : ``
   }
+
+  story1 = {
+    title : `We are the FHCC`,
+    content : `story 1`
+  }
+
+  story2 = {
+    title : `Thanks, Boulder Climbing Community!`,
+    content : `story 2`
+  }
+
+  story3 = {
+    title : `Adopt-a-Highway`,
+    content : `story 2`
+  }
+
+  story4 = {
+    title : `Cannonball Community Night`,
+    content : `story 2`
+  }
+
+  story5 = {
+    title : `Vandemonium`,
+    content : `story 2`
+  }
+
+  story6 = {
+    title : `Wag Bags`,
+    content : `story 2`
+  }
+
+
+  ngOnInit() {
+
+    this.featureStory = this.story1;
+
+  }
+
+
+  onStoryChange(story) {
+
+    this.featureStory = story;
+
+  }
+
 
 }
