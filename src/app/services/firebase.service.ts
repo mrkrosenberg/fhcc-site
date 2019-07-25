08bb7495ef52a469c0bf3d0ea390f913cd78c464
Mark <mrk.rosenberg@gmail.com>
2019-07-25 13:13:54 -0600

    configures/adds functionality to story service

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-// import {  }
+import { Story } from '../models/story';
 
 // Firebase
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
@@ -8,6 +8,18 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 @Injectable()
 export class FirebaseService {
 
-  constructor() { }
+  storyCollection: AngularFirestoreCollection<Story>;
+
+  story: Observable<Story[]>;
+
+  constructor(public firestore: AngularFirestore) { 
+
+    this.story = this.firestore.collection('story').valueChanges();
+
+  }
+
+  getStories() {
+    return this.story;
+  }
 
 }
