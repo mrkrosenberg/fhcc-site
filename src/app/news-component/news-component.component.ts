3e6ef45d5c998e7e4985345cb61c86e787cffafd
Mark <mrk.rosenberg@gmail.com>
2019-07-25 12:59:03 -0600

    initial transfer from private gitlab to public github, basic firebase functionality, model/interface for story objects

--- src/app/news-component/news-component.component.ts
+++ src/app/news-component/news-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Story } from '../models/story';
 
 @Component({
   selector: 'app-news-component',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private story: Story) { }
 
   featureStory = {
     title : ``,
