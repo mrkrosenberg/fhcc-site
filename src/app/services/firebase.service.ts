28f5210d7b902f96f9a9aafb4ca75715ec48b57f
Mark <mrk.rosenberg@gmail.com>
2019-08-03 10:01:58 -0600

    reverting to older commit

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Story } from '../models/story';
+// import {  }
 
 // Firebase
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
@@ -8,18 +8,6 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 @Injectable()
 export class FirebaseService {
 
-  storyCollection: AngularFirestoreCollection<Story>;
-
-  story: Observable<Story[]>;
-
-  constructor(public firestore: AngularFirestore) { 
-
-    this.story = this.firestore.collection('story').valueChanges();
-
-  }
-
-  getStories() {
-    return this.story;
-  }
+  constructor() { }
 
 }
